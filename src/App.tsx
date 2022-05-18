33f1e028f06635c137e6713ea0da810fdb11fd80
Bwhitt945 <71101855+Bwhitt945@users.noreply.github.com>
2022-05-18 13:35:31 -0500

    first website

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='crawford_logo-1.png'></img>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Test Web app
+        </p>
+        <a
+          className="App-link"
+          href="https://crawford-consulting.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Greg
+        </a>
+
+        <form>
+          <label>
+            Check: <input type = "text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </header>
+    </div>
+
+  );
+}
+
+export default App;
